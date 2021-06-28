3b3b194f26b54911225e2dfd09fe291a3b60cf2f
aman solanki <amansolanki125@gmail.com>
2021-06-28 11:47:03 +0530

    Initial commit

---
+++ src/main/scala/problem_4.scala
@@ -0,0 +1,11 @@
+object problem_4 {
+  def main(args: Array[String]): Unit = {
+  val list = List (2, -4, 3, -1, 23, -4, -54)
+    absolute(list)
+  }
+  def absolute(list: List[Int])={
+    for (num<-list)
+      println(num.abs)
+  }
+
+}
---
+++ src/main/scala/problem_7.scala
@@ -0,0 +1,12 @@
+object problem_7 {
+  def main(args: Array[String]): Unit = {
+    val input = Array(2, 3, 1, 7, 9, 5, 11, 3, 5)
+    max_difference(input)
+  }
+  def max_difference(arr: Array[Int])={
+    val max_element = arr.max
+    val min_element = arr.min
+    val max_diff = max_element - min_element
+    println(s"miximum difference: $max_diff")
+  }
+}
